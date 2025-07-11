5f677b3e40ef3dde0b9a1e38ea9d07c62aa8566b
dan-ik <danil00efimov@gmail.com>
2025-07-11 16:02:10 +0300

    firt project commit

---
+++ week0-setup/hello-csharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+//Console.WriteLine("Привет, Danil");
+int a = 13;
+int b = 9;
+int c = 0;
+string name = "Danil";
+void Greet(string name)
+{
+    Console.WriteLine($"Привет, {name}");
+}
+
+if (a < b)
+{
+    Console.WriteLine("a меньше b");
+}
+else (a > b)
+{
+    Console.WriteLine("a больше b");
+    int Sum(int a, int b)
+    {
+        return a + b;
+    }
+    Console.WriteLine();
+}
